eff0179e6fc993dd303ebf4969582cab78c19b3e
Juan Carlos Aranda <ncie.proyectos@gmail.com>
2021-09-27 09:31:36 -0600

    added user-articles relation and add rest routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,20 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
-  get "articles", to: "articles#index"
-  get "articles/new", to: "articles#new", as: :new_articles
-  get "articles/:id", to: "articles#show"
-  get "articles/:id/edit", to: "articles#edit"
+  # get "articles/user/:user_id", to: "articles#from_author"
 
-  patch "articles/:id", to: "articles#update", as: :article 
-  post "articles", to: "articles#create"
-  delete "articles/:id", to: "articles#destroy"
+
+  resources :articles do
+    get "user/:user_id", to: "articles#from_author", on: :collection
+  end
+
+  # CONVERT TO REST
+  #get "articles", to: "articles#index"
+  #get "articles/new", to: "articles#new", as: :new_articles
+  #get "articles/:id", to: "articles#show"
+  #get "articles/:id/edit", to: "articles#edit", as: :edit_articles
+
+  #patch "articles/:id", to: "articles#update", as: :article 
+  #post "articles", to: "articles#create"
+  #delete "articles/:id", to: "articles#destroy"
 end
